ba3c5d6ffcc1979f881ae7c7facb0deb0cf0d6ab
dokea <79191559+lee1205@users.noreply.github.com>
2022-04-21 23:51:53 +0800

    虎牙直播信息爬取

--- huyaspider.py
+++ huyaspider.py
@@ -1,22 +1,82 @@
 import requests
-from fake_useragent import UserAgent
-from lxml import etree
+from queue import Queue
+from threading import Thread
+import time
+
+class Huya_spider(Thread):
+
+    def __init__(self, data_queue, url):
+        Thread.__init__(self)
+        self.url = url
+        self.data_queue = data_queue
 
 
-url = 'https://www.huya.com/l'
-headers = {
-    'UserAgent': UserAgent().random
-}
-html = requests.get(url, headers).text
-e = etree.HTML(html)
-hlist = e.xpath('//ul[@class="live-list clearfix"]/li')
-with open('huyaspider.txt', 'a', encoding='utf8') as f:
-    f.write('标题' + '\t' + '主播名称' + '\t' + '直播类型' + '\t' + '观看热度' + '\t' + '头像' + '\t' + '直播间路径' + '\n')
-    for res in hlist:
-        title = res.xpath('a[2]/@title')[0]
-        name = res.xpath('span[@class="txt"]/span[1]/i/@title')[0]
-        type = res.xpath('span[@class="txt"]/span[2]/a/@title')[0]
-        onlinenum = res.xpath('span[@class="txt"]/span[3]/i[2]/text()')[0]
-        icon = res.xpath('span[@class="txt"]/span[1]/img/@data-original')[0]
-        href = res.xpath('a[1]/@href')[0]
-        f.write(title + '\t' + name + '\t' + type + '\t' + onlinenum + '\t' + icon + '\t' + href + '\n')
+    def run(self):
+        self.get_data()
+        self.parse()
+
+    def get_data(self):
+        while self.url.empty() == False:
+            res = requests.get(self.url.get()).json()
+            self.data_queue.put(res)
+            if res['data']['datas'] == []:
+                break
+            elif res['data']['datas'] == [] is False:
+                continue
+
+    def parse(self):
+        with open('huya.txt', 'a', encoding='UTF-8') as fp:
+            while self.data_queue.empty() == False:
+                res = self.data_queue.get()
+                datas = res['data']['datas']
+                for data in datas:
+                    item = {}
+                    item['主播名称'] = data['nick']
+                    item['标题'] = data['introduction']
+                    item['观看人数'] = data['totalCount']
+                    item['直播路径'] = data['gameHostName']
+                    item['直播类型'] = data['gameFullName']
+                    item['直播间id'] = data['profileRoom']
+                    item['主播头像'] = data['avatar180']
+                    item['封面'] = data['screenshot']
+                    fp.write(str(item))
+
+
+
+
+if __name__ == '__main__':
+    start = time.time()#开始时间
+    data_queue = Queue()
+    url = Queue()
+    baseurl = 'https://www.huya.com/cache.php?m=LiveList&page={}'
+    for i in range(500):
+        url.put(baseurl.format(i))
+
+    crawl = []
+
+    for i in range(4):
+        douyu = Huya_spider(data_queue=data_queue, url=url)
+        crawl.append(douyu)
+        douyu.start()
+
+    for douyu in crawl:
+        douyu.join()
+
+    end = time.time()#结束时间
+    t = end - start
+    print(t, '秒')
+
+"""
+item['主播名称'] = x['nick']
+item['标题'] = x['introduction']
+item['观看人数'] = x['totalCount']
+item['直播路径'] = x['gameHostName']
+item['直播类型'] = x['gameFullName']
+item['直播间id'] = x['profileRoom']
+item['主播头像'] = x['avatar180']
+item['封面'] = x['screenshot']
+
+"""
+
+
+
